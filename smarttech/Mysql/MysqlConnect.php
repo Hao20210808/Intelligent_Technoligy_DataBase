edfe39940b14a3be7f0470357cd2b052951d27e1
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-10-09 13:38:01 +0800

    Add files via upload

---
+++ smarttech/Mysql/MysqlConnect.php
@@ -0,0 +1,19 @@
+<?php
+  $host = 'localhost';
+  $dbuser = 'root';
+  $dbpw = 'm1V@81rQweM4*A/x';
+  $dbname = 'smarttech';
+
+  $link = mysqli_connect($host, $dbuser, $dbpw, $dbname);
+  if ($link) {
+    //if return value > 0, then connected successful
+    //code => UTF-8
+    //mysql_query($link, $query)
+    mysql_query($link, "SET NAMES utf8")
+    echo "connected success";
+  } else {
+    //if fail，connected failed, return error
+    //mysql_connect_error();
+    echo 'connected fail :<br/>' . mysqli_connect_error();
+  }
+?>
